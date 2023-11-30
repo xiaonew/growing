afbcb20f74573fa6f10e8ed3245f6f069f2c5201
liuhuan <liuhuan6165@100.me>
2023-12-01 06:52:17 +0800

    git ignore

---
+++ jdkbase/src/main/java/com/gs/Main.java
@@ -0,0 +1,11 @@
+package com.gs;
+
+/**
+ * @author: huan.liu
+ * @since : 2023-12-01
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
